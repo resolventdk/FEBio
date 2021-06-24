da8c056c860e93d2a7b181de7d0baa4c92ff61f0
spietz <henrikspietz@gmail.com>
2021-06-24 13:39:54 +0800

    negjac plugin dumps domain name and position

--- Plugins/DumpNegJac/src/negjac.cpp
+++ Plugins/DumpNegJac/src/negjac.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <string>
 #include <vector>
-#include <algorithm>
 
 #include <FECore/sdk.h>
 #include <FECore/Callback.h>
@@ -23,28 +21,24 @@ public:
 			// Model is solved
 
 			// get "domains"
-			FEMesh& m = fem.GetMesh();
-			int ND = m.Domains();
+			FEMesh& mesh = fem.GetMesh();
+			int ND = mesh.Domains();
 			
-			// loop domains collect the negative jacobians
-			vector<int> negJacElems;
-			for (int i = 0; i < ND; ++i)
-			{
-				FEDomain& dom = m.Domain(i);
-				for (vector<int>::iterator it = dom.m_NegJacElems.begin(); it != dom.m_NegJacElems.end(); ++it)
-					negJacElems.push_back(*it);
-
-			}
-			
-			// sort
-			sort(negJacElems.begin(), negJacElems.end());
-
-			// dump to file
+			// loop domains get the negative jacobians
+            // dump to file
 			FILE* pFile;
 			pFile = fopen("negjac.dat", "w");
-			for (vector<int>::iterator it = negJacElems.begin(); it != negJacElems.end(); ++it)
-				fprintf(pFile, "%d\n", *it);
-
+			for (int i = 0; i < ND; ++i)
+			{
+				FEDomain& dom = mesh.Domain(i);
+				string name = dom.GetName();
+				for (vector<int>::iterator it = dom.m_NegJacElems.begin(); it != dom.m_NegJacElems.end(); ++it){
+					int eid = *it;
+					FEElement* el = mesh.Element(eid);
+					vec3d pos = mesh.Node(el->m_node[0]).m_rt;
+					fprintf(pFile, "%d, %s, %f, %f, %f\n", eid, name.c_str(), pos.x, pos.y, pos.z);
+				}
+			}
 			printf("done dumping negative jacobians to negjac.json\n");
 
         }
